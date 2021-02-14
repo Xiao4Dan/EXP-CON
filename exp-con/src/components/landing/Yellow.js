f6930e3a7a8e9980c8dcabc374361eed8743af00
Xiao4Dan <alexshi416@gmail.com>
2021-02-14 13:13:55 -0500

    landing red and styling

--- exp-con/src/components/landing/Yellow.js
+++ exp-con/src/components/landing/Yellow.js
@@ -1,8 +1,15 @@
 import React from "react";
+import yellow from '../../images/yellow.png';
 function Yellow(props) {
 
   return (
-    <div className="">
+    <div className="YellowSection">
+      <button id="tryBtn" className="normalBtn">TRY IT NOW</button>
+      <div className="redHeading">
+        <h1 id="heading" className="heading">FRONT ROW SEATS</h1>
+        <h2 id="subHeading" className="subHeading">Experience concerts up close and personal.</h2>
+        <button id="demoBtn" className="normalBtn">SEE DEMO</button>
+      </div>
     </div>
   );
 }
